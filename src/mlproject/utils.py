eef0b53054014f7b7493313fd3e514cf93ab538c
Abhinav Sharma <abhinavsharma9000@gmail.com>
2026-01-29 13:10:21 +0530

    Data Ingestion

---
+++ src/mlproject/utils.py
@@ -0,0 +1,39 @@
+import os 
+import sys
+from src.mlproject.exception import CustomerException
+from src.mlproject.logger import logging
+import pymysql 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+import pandas as pd   
+from dotenv import load_dotenv
+import pickle
+
+load_dotenv()
+
+host=os.getenv("host") 
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv("db")
+
+
+
+def read_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+        mydb = pymysql.connect(
+             host=host,
+             user=user,
+             password=password, 
+             db=db
+        )
+
+        logging.info("connection establish",mydb)
+
+        df=pd.read_sql_query('Select * from raw',mydb)
+        print(df.head())
+
+        return df
+
+    except Exception as ex:
+        raise CustomerException(ex, sys) 
