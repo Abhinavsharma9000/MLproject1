ba8fee46c10ab2b3a76b5b5d8d3a255ec6d52489
Abhinav Sharma <abhinavsharma9000@gmail.com>
2026-02-04 13:54:56 +0530

    Data Transformation

--- src/mlproject/exception.py
+++ src/mlproject/exception.py
@@ -1,21 +1,21 @@
 import sys
 from src.mlproject.logger import logging
-from typing import Any
 
 
-def error_message_detail(error, error_detail): 
-    _,_, exc_tb=error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename 
+def error_message_detail(error,error_detail):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
 
-    error_message="Error occured in python script name [{0}] "\
-    "line number [{1}] error message [{2}] ".format(
-    file_name, exc_tb.tb_lineno, str(error)) 
-    
     return error_message
-class CustomerException(Exception):
-    def __init__(self,error_message,error_details): 
+
+
+class CustomException(Exception):
+    pass
+    def __init__(self,error_message,error_details):
         super().__init__(error_message)
-        self.error_messgae=error_message_detail(error_message,error_details)
+        self.error_message=error_message_detail(error_message,error_details)
 
     def __str__(self):
-        return self.error_messgae
+        return self.error_message
